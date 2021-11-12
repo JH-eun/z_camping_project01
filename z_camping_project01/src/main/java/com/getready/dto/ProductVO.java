f3b506341b3eb67747f7e378962fa2f269a2860d
jieunhur <jieunhur@localhost>
2021-11-12 19:17:41 +0900

    add String image

--- z_camping_project01/src/main/java/com/getready/dto/ProductVO.java
+++ z_camping_project01/src/main/java/com/getready/dto/ProductVO.java
@@ -11,6 +11,13 @@ public class ProductVO {
 	private int price2;
 	private int price3;
 	private String content;
+	private String image;
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
 	private String puseyn;
 	private String bestyn;
 	private Timestamp indate;
