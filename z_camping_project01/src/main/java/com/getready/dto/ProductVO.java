5bc2bd476ff0d15ea949df31c5b4a5b870eda319
Jieun <Jieun@localhost>
2021-11-12 18:21:36 +0900

    2nd commit

---
+++ z_camping_project01/src/main/java/com/getready/dto/ProductVO.java
@@ -0,0 +1,79 @@
+package com.getready.dto;
+
+import java.sql.Timestamp;
+//상품정보 저장
+public class ProductVO {
+
+	private int pnum;
+	private String pname;
+	private String kind;
+	private int price1;
+	private int price2;
+	private int price3;
+	private String content;
+	private String puseyn;
+	private String bestyn;
+	private Timestamp indate;
+	
+	public int getPnum() {
+		return pnum;
+	}
+	public void setPnum(int pnum) {
+		this.pnum = pnum;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getKind() {
+		return kind;
+	}
+	public void setKind(String kind) {
+		this.kind = kind;
+	}
+	public int getPrice1() {
+		return price1;
+	}
+	public void setPrice1(int price1) {
+		this.price1 = price1;
+	}
+	public int getPrice2() {
+		return price2;
+	}
+	public void setPrice2(int price2) {
+		this.price2 = price2;
+	}
+	public int getPrice3() {
+		return price3;
+	}
+	public void setPrice3(int price3) {
+		this.price3 = price3;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getPuseyn() {
+		return puseyn;
+	}
+	public void setPuseyn(String puseyn) {
+		this.puseyn = puseyn;
+	}
+	public String getBestyn() {
+		return bestyn;
+	}
+	public void setBestyn(String bestyn) {
+		this.bestyn = bestyn;
+	}
+	public Timestamp getIndate() {
+		return indate;
+	}
+	public void setIndate(Timestamp indate) {
+		this.indate = indate;
+	}
+	
+}
