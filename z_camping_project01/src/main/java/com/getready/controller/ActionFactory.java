2e959d3a09b23b4b726ad7898050ba9a67c33ff2
Jieun <Jieun@localhost>
2021-11-16 17:48:00 +0900

    11th modify ActionFactory, add sub_image.html,sub_menu.html, productDetail.jsp

--- z_camping_project01/src/main/java/com/getready/controller/ActionFactory.java
+++ z_camping_project01/src/main/java/com/getready/controller/ActionFactory.java
@@ -4,20 +4,23 @@ import com.getready.controller.action.*;
 
 public class ActionFactory {
 	private static ActionFactory instance = new ActionFactory();
+
 	public ActionFactory() {
 		super();
 	}
-	
+
 	public static ActionFactory getInstance() {
 		return instance;
 	}
-	
+
 	public Action getAction(String command) {
 		Action action = null;
 		System.out.println("ActionFactory : " + command);
-		
-		if(command.equals("index")) {
+
+		if (command.equals("index")) {
 			action = new IndexAction();
+		} else if (command.equals("product_deatil")) {
+			action = new ProductDetailAction();
 		}
 		return action;
 	}
